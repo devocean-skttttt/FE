c02cd5fdfbf2195d1f7a2680d7fe15c248924765
seondal <sseondal@gmail.com>
2024-10-03 19:15:06 +0900

    💄 style :  바텀 네비게이션 아이콘

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,52 +1,40 @@
 'use client';
 
-import Link from 'next/link';
 import ICON from '@/constants/ICON';
-import { usePathname } from 'next/navigation';
 import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const BOTTOM_MENU = [
-  { path: '/', text: '홈', icon: ICON.nav.home },
-  { path: '/upload', text: '등록하기', icon: ICON.nav.upload },
-  { path: '/mypage', text: '마이페이지', icon: ICON.nav.mypage },
+  { path: '/', text: '탐색', icon: ICON.nav.home },
+  { path: '/upload', text: '등록', icon: ICON.nav.upload },
+  { path: '/mypage', text: '마이', icon: ICON.nav.mypage },
 ] as const;
 
-function BottomNavigation() {
+const BottomNavigation = () => {
   const pathname = usePathname();
 
   const isActive = (path: string) => pathname === path;
 
   return (
     <>
-      <div className="flex w-full items-center h-12 justify-around fixed bottom-0 bg-white max-w-mobile text-center">
-        {BOTTOM_MENU.map(({ path, text, icon }) => {
-          const isActiveItem = isActive(path);
-          const activeClass = isActiveItem
-            ? 'text-[#330218]'
-            : 'text-[#999999]';
-          const iconColor = isActiveItem ? '#330218' : '#999999';
-
-          return (
-            <Link href={path} key={path}>
-              <div className="w-20 flex flex-col items-center">
-                {icon && (
-                  <Image
-                    src={icon}
-                    alt="icon"
-                    width={24}
-                    height={24}
-                    style={{ color: iconColor }}
-                  />
-                )}
-                {text && <p className={`text-xs ${activeClass}`}>{text}</p>}
-              </div>
-            </Link>
-          );
-        })}
-      </div>
+      <nav className="flex w-full items-center h-12 justify-around fixed bottom-0 bg-white max-w-mobile">
+        {BOTTOM_MENU.map(({ path, text, icon }) => (
+          <Link href={path} key={path}>
+            <div className="w-20 flex flex-col items-center">
+              <Image src={icon} alt={text} width={20} height={20} />
+              <span
+                className={isActive(path) ? 'text-[#330218]' : 'text-[#999999]'}
+              >
+                {text}
+              </span>
+            </div>
+          </Link>
+        ))}
+      </nav>
       <div className="h-[54px]" />
     </>
   );
-}
+};
 
 export default BottomNavigation;
